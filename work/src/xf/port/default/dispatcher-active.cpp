b8187f098602c1c37ea17a0d3ddf313932935f80
yanMichellod <yan.michellod@hotmail.com>
2019-02-26 07:54:51 +0100

    update file

--- work/src/xf/port/default/dispatcher-active.cpp
+++ work/src/xf/port/default/dispatcher-active.cpp
@@ -18,12 +18,70 @@ using interface::XFResourceFactory;
 
 // TODO: Implement code for XFDispatcherActive class
 
-#endif // USE_XF_DISPATCHER_ACTIVE_DEFAULT_IMPLEMENTATION
 
-XFDispatcherActiveDefault::XFDispatcherActiveDefault()
+
+XFDispatcherActiveDefault::XFDispatcherActiveDefault():_events()
 {
     _bInitialized = false;
     _bExecuting = false;
-    //_events = ;
-    //_pMutex = new interface::XFMutex();
+    _pMutex = interface::XFResourceFactory::getInstance()->createMutex();
+    _pThread = interface::XFResourceFactory::getInstance()->createThread();
 }
+
+XFDispatcherActiveDefault::~XFDispatcherActiveDefault()
+{
+    delete _pMutex;
+    delete _pThread;
+}
+
+void XFDispatcherActiveDefault::initialize()
+{
+    _bInitialized = true;
+}
+
+void XFDispatcherActiveDefault::start()
+{
+    _pThread->start();
+    _bExecuting = true;
+}
+
+void XFDispatcherActiveDefault::stop()
+{
+    _pThread->stop();
+    _bExecuting = false;
+}
+
+void XFDispatcherActiveDefault::pushEvent(XFEvent *pEvent)
+{
+    _events.push(pEvent);
+}
+
+void XFDispatcherActiveDefault::scheduleTimeout(int timeoutId, int interval, interface::XFReactive *pReactive)
+{
+
+}
+
+void XFDispatcherActiveDefault::unscheduleTimeout(int timeoutId, interface::XFReactive *pReactive)
+{
+
+}
+
+int XFDispatcherActiveDefault::executeOnce()
+{
+    /// can not be called if start() has been executed
+    if(_bExecuting == true){
+        return -1;
+    }
+}
+
+int XFDispatcherActiveDefault::execute(const void *param)
+{
+
+}
+
+void XFDispatcherActiveDefault::dispatchEvent(const XFEvent *pEvent) const
+{
+
+}
+
+#endif // USE_XF_DISPATCHER_ACTIVE_DEFAULT_IMPLEMENTATION
