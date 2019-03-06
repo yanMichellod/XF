c8b3f4c5e120247c71fd89254412db085b9d8d52
yanMichellod <39124209+yanMichellod@users.noreply.github.com>
2019-03-06 12:09:34 +0100

    update version

--- work/src/xf/port/default/eventqueue-default.cpp
+++ work/src/xf/port/default/eventqueue-default.cpp
@@ -13,4 +13,21 @@ XFEventQueueDefault::XFEventQueueDefault(){
 virtual XFEventQueueDefault::~XFEventQueueDefault(){
 
 }
+
+virtual bool XFEventQueueDefault::empty() const{
+	///< Returns true if no event is in the queue.
+}
+virtual bool XFEventQueueDefault::push(const XFEvent * pEvent){
+	///< Pushes the given event onto the queue.
+}
+virtual const XFEvent * XFEventQueueDefault::front(){
+	///< Returns pointer to next event to pop.
+}
+virtual void XFEventQueueDefault::pop(){
+	///< Pops the next event from the queue.
+}
+                                                       ///< Wait for the next event to arrive. Returns true if an event is in the queue.
+virtual bool XFEventQueueDefault::pend(){
+
+}
 #endif // USE_XF_EVENT_QUEUE_DEFAULT_IMPLEMENTATION
